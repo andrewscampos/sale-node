67dbb6ecd242cb6bbfe49fce767fb93ded359678
Andrews Balbino Campos <andrews.campos@hotmail.com>
2018-05-02 14:39:03 -0300

    nova funcionalidades

--- public/customActivity.js
+++ public/customActivity.js
@@ -30,11 +30,13 @@ define(function (require) {
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        if (inArguments[0].message ) {
+        if (inArguments[0].shortMessage ) {
             $('#type').val(inArguments[0].type);
             $('#title').val(inArguments[0].title);
 			$('#message').val(inArguments[0].message);
 			$('#categoria').val(inArguments[0].categoria);
+			$('#shortMessage').val(inArguments[0].shortMessage);
+			$('#rastreamento').val(inArguments[0].rastreamento);
         }
     }
 
@@ -80,9 +82,11 @@ define(function (require) {
 		payload.name = $('#type').val();
 
 		payload['arguments'].execute.inArguments[0].type = $('#type').val();
-        payload['arguments'].execute.inArguments[0].title = $('#title').val();
+    payload['arguments'].execute.inArguments[0].title = $('#title').val();
 		payload['arguments'].execute.inArguments[0].message = $('#message').val();
 		payload['arguments'].execute.inArguments[0].categoria = $('#categoria').val();
+		payload['arguments'].execute.inArguments[0].shortMessage = $('#shortMessage').val();
+		payload['arguments'].execute.inArguments[0].rastreamento = $('#rastreamento').val();
 		payload['metaData'].isConfigured = true;
 
 		connection.trigger('updateActivity', payload);
